9770e92e88e4d1d729bd6efa40c31b755b56a5ac
Rich Olson <rich@zenzig.com>
2022-12-23 17:09:16 +0000

    first commit

---
+++ checkStream.js
@@ -0,0 +1,55 @@
+const { spawn } = require('child_process');
+
+// Async function to check a stream using ffprobe
+async function checkStream(url) {
+  // Return a new Promise
+  return new Promise((resolve, reject) => {
+    // Spawn the ffprobe command with the given arguments
+    const ffprobe = spawn('ffprobe', [
+      '-v', 'error',
+      '-show_entries', 'stream=codec_name,duration',
+      '-of', 'default=noprint_wrappers=1',
+      url,
+    ]);
+
+    // Initialize variables to track whether codec and duration information was found
+    let codecsFound = false;
+    let durationFound = false;
+
+    // When data is received from ffprobe's stdout, check for codec and duration information
+    ffprobe.stdout.on('data', (data) => {
+      const output = data.toString();
+      if (output.includes('codec_name')) {
+        codecsFound = true;
+        console.log('codecs: true');
+      }
+      if (output.includes('duration')) {
+        durationFound = true;
+        console.log('duration: true');
+      }
+
+    });
+
+    // When ffprobe closes, check if codec and duration information was found
+    ffprobe.on('close', (code) => {
+      // If codec and duration information was found, resolve the Promise with true
+      if (codecsFound && durationFound) {
+        resolve(true);
+      } else {
+        // Otherwise, resolve the Promise with false
+        resolve(false);
+      }
+      // Kill the ffprobe process
+      ffprobe.kill();
+    });
+
+    // Set a timeout of 10 seconds
+    setTimeout(() => {
+      // If the timeout is reached, reject the Promise with an error
+      reject(new Error('Timed out'));
+    }, 15000);
+  });
+}
+
+module.exports = checkStream;
+
---
+++ readme.md
@@ -0,0 +1,26 @@
+# checkStream
+## checkStream is an async function that determines if an RTSP stream is running by using the ffprobe command to check for codec and duration information.
+
+## Installation
+To use checkStream, you will need to have ffmpeg installed on your system.
+
+## Usage
+```
+const checkStream = require('checkStream');
+
+async function main() {
+  try {
+    const result = await checkStream('rtsp://myStreamDomain.com:554/profile2/media.smp');
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
+```
+## Output
+If codec and duration information is found in the stream, checkStream will return true. If the information is not found, or if the ffprobe command times out, checkStream will return false or throw an error, respectively.
+
+## License
+This project is licensed under the MIT License.
